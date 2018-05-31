514f6abd6d3db73394d1f0a815e4019baf544533
ta7sudan <ta7sudan@gmail.com>
2018-05-31 17:49:17 +0800

    add note

--- js/base64/index.js
+++ js/base64/index.js
@@ -1,4 +1,5 @@
 // from https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+// note: 实际使用 decode 时记得 try-catch, atob 对非 4 的整数倍字符串解码会抛出异常
 
 function encodeBase64(str) {
 	return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (m, p) {

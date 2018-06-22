d682abd9f75338df36ab4f53ffc39e8e489c8ebd
ta7sudan <ta7sudan@gmail.com>
2018-06-22 15:52:55 +0800

    remove class

--- js/inherit/demo0.js
+++ js/inherit/demo0.js
@@ -1,30 +1,24 @@
-function Person(name) {
+function Person(name, age) {
 	var self = this instanceof Person ? this : Object.create(Person.prototype);
 	// public variable
 	self.name = name;
-	// private variable
-	var age = 10;
+	self.age = age;
 
-	// private instance method can get this or other private variable but use more memory
-	function greet(name) {
-		console.log(`Hello, ${name}, I'm ${self.name}, I'm ${age} years old.`);
-	}
-
-	// public instance method
+	// public method
 	if (typeof self.sayHello !== 'function') {
 		Person.prototype.sayHello = function (name) {
-			greet(name);
+			console.log(`Hello, ${name}, I'm ${this.name}, I'm ${this.age} years old.`);
 		};
 	}
 
 	return self;
 }
 
-function Student(name, book) {
+function Student(name, age, book) {
 	var self = this instanceof Student ? this : Object.create(Student.prototype);
 
 	// 我继承一个 class, 却需要知道这个 class 的构造函数的参数, 好像有点说不过去
-	Person.call(self, name);
+	Person.call(self, name, age);
 
 	self.book = book;
 
@@ -44,3 +38,12 @@ function inheritPrototype(subType, superType) {
 }
 
 inheritPrototype(Student, Person);
+
+var a = new Student('aaa', 16, 'SICP');
+var b = new Student('bbb', 18, 'CSAPP');
+console.log(a.name);
+console.log(a.age);
+console.log(a.book);
+console.log(b.name);
+console.log(b.age);
+console.log(b.book);
